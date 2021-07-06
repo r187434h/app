a10d56c08fc800f705b369153692e1b09a9482c1
r187434h <86804713+r187434h@users.noreply.github.com>
2021-07-06 20:22:47 +0200

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+import os
+import aiml
+from flask import Flask 
+
+BRAIN_FILE="brain.dump"
+
+k = aiml.Kernel()
+
+if os.path.exists(BRAIN_FILE):
+    print("Loading from brain file: " + BRAIN_FILE)
+    k.loadBrain(BRAIN_FILE)
+else:
+    print("Parsing aiml files")
+    k.bootstrap(learnFiles="std-startup.aiml", commands="load aiml b")
+    print("Saving brain file: " + BRAIN_FILE)
+    k.saveBrain(BRAIN_FILE)
+
+while True:
+    input_text = input("bot>>> ")
+    response = k.respond(input_text)
+    print(response)
+
+
